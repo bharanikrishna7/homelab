b2fa989c198e70c4b9cb763823eae7cc80127419
Bharani Krishna <bharanikrishna7@gmail.com>
2025-11-25 21:15:30 +0530

    wrapper script to bring up tailscale container

---
+++ ruby/tailscale/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+help() {
+  echo ""
+  echo "Usage: $0 <interface> <authkey>"
+  echo -e "  interface : Interface can be found by running 'ip a'"
+  echo -e "  authkey   : Authkey can be generated using tailscale website and navigating to settings->keys"
+  exit 1 # Exit script after printing help
+}
+
+# check the number of arguments
+# ensure that exactly 1 argument is supplied
+if [ $# -ne 3 ]; then
+  # code to be executed if the condition is true
+  printf "this script requires exactly one parameter to be supplied, but supplied : $# \n"
+  help
+fi
+
+# interface is captured from agruments
+interface=$1
+
+# CIDR associated with supplied interface
+cidr=$(eval bash ../scripts/cidr.sh $interface)
+cidr_size=${#cidr} # should be <= 18 for valid CIDR value
+
+# check the number of arguments
+# ensure that exactly 1 argument is supplied
+if [ $cidr_size -gt 18 ]; then
+  # code to be executed if the condition is true
+  echo $cidr
+  exit 1 # Exit script after printing help
+fi
+
+# authkey also captured from arguments
+authkey=$2
+
+# bring up the tailscale docker compose
+# with CIDR of supplied interface in SubNet routes
+# this will enable accessing devices connected to host OS
+TS_ROUTES="$cidr" AUTHKEY=$authkey HOSTNAME=$HOSTNAME docker compose up --detach
