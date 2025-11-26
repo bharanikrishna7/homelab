2ef6c264cb32d32a654bf8257eeadf5d0c9a2910
Bharani Krishna <bharanikrishna7@gmail.com>
2025-11-26 18:01:34 +0530

    using host network directly

--- ruby/tailscale/run.sh
+++ ruby/tailscale/run.sh
@@ -2,44 +2,24 @@
 
 help() {
   echo ""
-  echo "Usage: $0 <interface> <authkey>"
-  echo -e "  interface : Interface can be found by running 'ip a'"
+  echo "Usage: $0 <authkey>"
   echo -e "  authkey   : Authkey can be generated using tailscale website and navigating to settings->keys"
   exit 1 # Exit script after printing help
 }
 
 # check the number of arguments
 # ensure that exactly 1 argument is supplied
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   # code to be executed if the condition is true
   printf "this script requires exactly one parameter to be supplied, but supplied : $# \n"
   help
 fi
 
-# interface is captured from agruments
-interface=$1
-
-# CIDR associated with supplied interface
-cidr=$(eval bash ../scripts/cidr.sh $interface)
-cidr_size=${#cidr} # should be <= 18 for valid CIDR value
-echo "CIDR on interface \"$interface\" : $cidr"
+# authkey, captured from arguments
+authkey=$1
 
 
-# check the number of arguments
-# ensure that exactly 1 argument is supplied
-if [ $cidr_size -gt 18 ]; then
-  # code to be executed if the condition is true
-  echo $cidr
-  exit 1 # Exit script after printing help
-fi
-
-echo "Will Add Subnet : $cidr to tailscale to enable accessing devices connected to host's network"
-
-# authkey also captured from arguments
-authkey=$2
-
-echo "Starting Tailscale..."
 # bring up the tailscale docker compose
-# with CIDR of supplied interface in SubNet routes
-# this will enable accessing devices connected to host OS
-TS_ROUTES="$cidr" AUTHKEY=$authkey HOSTNAME=$HOSTNAME docker compose up --detach
+echo "Starting Tailscale..."
+AUTHKEY=$authkey HOSTNAME=$HOSTNAME docker compose up --detach
+echo "Tailscale is up..."
