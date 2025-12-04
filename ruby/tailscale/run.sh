ea67874c321748267d0dff47bb82f6bdefb9a8f2
Bharani Krishna <bharanikrishna7@gmail.com>
2025-12-04 06:55:40 +0530

    simplified run.sh file

--- ruby/tailscale/run.sh
+++ ruby/tailscale/run.sh
@@ -2,25 +2,47 @@
 
 help() {
   echo ""
-  echo "Usage: $0 <authkey>"
-  echo -e "  authkey   : Authkey can be generated using tailscale website and navigating to settings->keys"
+  echo "Usage: $0 [help]"
+  echo -e "  help   : prints script usage"
   exit 1 # Exit script after printing help
 }
 
 # check the number of arguments
-# ensure that exactly 1 argument is supplied
-if [ $# -ne 1 ]; then
+# ensure that <= 1 argument is supplied
+if [ $# -gt 1 ]; then
   # code to be executed if the condition is true
-  printf "this script requires exactly one parameter to be supplied, but supplied : $# \n"
+  printf "this script requires <= 1 parameter to be supplied, but supplied : $# \n"
   help
 fi
 
-# authkey, captured from arguments
-authkey=$1
+# if exactly 1 argument is supplied
+if [ $# -eq 1 ]; then
+  help
+  # check if param is help
+  if [ $1 == "help" ]; then
+    exit 0
+  # illegal param supplied
+  else
+    exit 1
+  fi
+fi
 
+# Tailscale state directory needs to be cleaned up before 
+# manually bringing up new tailscale container
+echo "cleaning up tailscale directories..."
+echo "  -> if it's desired to preserve state directory, to not regenerate single use key"
+echo "     everytime. Please use 'docker compose command'  shown below. It will preserve state."
+echo "              docker compose up --detach"
+rm -rf /var/lib/tailscale
+
+if [ -e "/var/lib/tailscale" ]; then
+  echo "unable to delete file, check permissions"
+  exit 1
+fi
 
 # bring up the tailscale docker compose
+echo "Please ensure parameters have been setup in .env file..."
 echo "Starting Tailscale..."
 docker compose pull
-AUTHKEY=$authkey HOST=$HOSTNAME docker compose up --detach
+docker compose up --detach
 echo "Tailscale is up..."
